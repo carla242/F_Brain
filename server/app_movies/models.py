72d8f7f90f2063b6cf1888dc9738162a4b293e8c
carla242 <njohcarla26@gmail.com>
2025-06-05 15:10:41 +0200

    Ajout de m'application app_movies dans la db django pour gérer lajout et suppression des films

--- server/app_movies/models.py
+++ server/app_movies/models.py
@@ -1,7 +1,12 @@
 # app_movies/models.py
 from django.db import models
+from django.conf import settings
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     trailer = models.URLField(blank=True)
+
+def __str__(self):
+        return self.title
--- server/app_movies/views.py
+++ server/app_movies/views.py
@@ -1,4 +1,3 @@
-# app_movies/views.py
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -11,15 +10,27 @@ def add_movie(request):
             data = json.loads(request.body)
             title = data.get('title')
             description = data.get('description')
-            trailer = data.get('trailer')
-
+            
             Movie.objects.create(
                 title=title,
-                description=description,
-                trailer=trailer
+                description=description    
             )
             return JsonResponse({'message': 'Film ajouté avec succès !'}, status=201)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
 
     return JsonResponse({'error': 'Méthode non autorisée'}, status=405)
+
+def get_movies(request):
+    if request.method == 'GET':
+        movies = list(Movie.objects.values())
+        return JsonResponse(movies, safe=False)
+
+def list_movies(request):
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        data = [
+            {"id": movie.id, "title": movie.title, "description": movie.description, "trailer": movie.trailer}
+            for movie in movies
+        ]
+        return JsonResponse(data, safe=False)
